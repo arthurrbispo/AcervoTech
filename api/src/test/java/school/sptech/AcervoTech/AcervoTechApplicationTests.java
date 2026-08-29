ef68622e8a20a5aee0ccf099fda843ed360abcd7
arthurrbispo <arthur.bsantos@sptech.school>
2026-08-28 21:40:16 -0300

    Adiciona a API ao diretório api

---
+++ api/src/test/java/school/sptech/AcervoTech/AcervoTechApplicationTests.java
@@ -0,0 +1,13 @@
+package school.sptech.AcervoTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcervoTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
