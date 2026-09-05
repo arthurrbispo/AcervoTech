d8ed4f1a2e7860f19111576b08be74b45f7091e4
arthurrbispo <arthur.bsantos@sptech.school>
2026-09-05 13:21:21 -0300

    Cria a classe Livro na API

---
+++ api/src/main/java/school/sptech/AcervoTech/Livro.java
@@ -0,0 +1,50 @@
+package school.sptech.AcervoTech;
+
+public class Livro {
+
+    private Integer id;
+    private String titulo;
+    private String autor;
+    private String categoria;
+    private Integer quantidade;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+}
---
+++ api/src/main/java/school/sptech/AcervoTech/LivroController.java
@@ -0,0 +1,4 @@
+package school.sptech.AcervoTech;
+
+public class LivroController {
+}
