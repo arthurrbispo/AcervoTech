567e21c4c62492d3053aafc5e0f43a4813ac41f5
arthurrbispo <arthur.bsantos@sptech.school>
2026-09-05 14:55:57 -0300

    Cria o Controller e o Schema do H2

--- api/src/main/java/school/sptech/AcervoTech/LivroController.java
+++ api/src/main/java/school/sptech/AcervoTech/LivroController.java
@@ -1,4 +1,68 @@
 package school.sptech.AcervoTech;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
+@CrossOrigin(origins = "http://localhost:5173")
+@RestController
+@RequestMapping("/livros")
 public class LivroController {
+
+    private final JdbcTemplate template;
+
+    public LivroController(JdbcTemplate template) {
+        this.template = template;
+    }
+
+    @PostMapping
+    public ResponseEntity<Livro> cadastrarLivro(@RequestBody Livro livro){
+        if (
+                livro.getTitulo() == null ||
+                        livro.getTitulo().isBlank() ||
+                        livro.getAutor() == null ||
+                        livro.getAutor().isBlank() ||
+                        livro.getCategoria() == null ||
+                        livro.getCategoria().isBlank() ||
+                        livro.getQuantidade() == null ||
+                        livro.getQuantidade() < 0
+        ) {
+            return ResponseEntity.status(400).build();
+        }
+
+        String sql = """
+                INSERT INTO livro (
+                titulo,
+                autor,
+                categoria,
+                quantidade)
+                VALUES (?, ?, ?, ?)
+                """;
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        template.update(con -> {
+            PreparedStatement statement = con.prepareStatement(
+                    sql,
+                    Statement.RETURN_GENERATED_KEYS
+            );
+
+            statement.setString(1, livro.getTitulo());
+            statement.setString(2, livro.getAutor());
+            statement.setString(3, livro.getCategoria());
+            statement.setInt(4, livro.getQuantidade());
+
+            return statement;
+
+        }, keyHolder);
+
+        livro.setId(keyHolder.getKey().intValue());
+
+        return ResponseEntity.status(201).body(livro);
+    }
 }
